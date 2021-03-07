42bc401a1b45e59ff3a0ac9892edc98cc4b85ae1
David Ten Sing <david10sing@hotmail.com>
2021-03-07 11:17:58 +0400

    Added storybook and fix react externals

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,22 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
-  devtool: 'inline-source-map',
+  externals: {      
+    // Don't bundle react or react-dom      
+    react: {          
+        commonjs: "react",          
+        commonjs2: "react",          
+        amd: "React",          
+        root: "React"      
+    },      
+    "react-dom": {          
+        commonjs: "react-dom",          
+        commonjs2: "react-dom",          
+        amd: "ReactDOM",          
+        root: "ReactDOM"      
+    }  
+  },
+  devtool: 'source-map',
   module: {
     rules: [
       {
@@ -56,9 +71,15 @@ module.exports = {
   ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
+    alias: {          
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),      
+    },
   },
   output: {
     filename: 'bundle.js',
+    library: 'react-scrollspy-io',
+    libraryTarget: 'umd',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
